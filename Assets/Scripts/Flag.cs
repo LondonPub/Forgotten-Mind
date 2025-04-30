757a968b883bb639f4b50cae1782ec5ae535db27
London Pub <thomasharruda@icloud.com>
2025-04-30 12:32:47 -0400

    ima kms

--- Assets/Scripts/Flag.cs
+++ Assets/Scripts/Flag.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Flag : MonoBehaviour
+public class MirrorsNShi : MonoBehaviour
 {
    private void OnTriggerEnter2D(Collider2D other)
    {
       if(other.gameObject.CompareTag("Mirror"))
       {
-        SceneManager.LoadScene("Level2");
+        SceneManager.LoadScene("Level 2");
+      }
+      if(other.gameObject.CompareTag("Mirror2"))
+      {
+        SceneManager.LoadScene("Level 3");
+      }
+      if(other.gameObject.CompareTag("Mirror3"))
+      {
+        SceneManager.LoadScene("Level 4");
       }
       if(other.gameObject.CompareTag("Win"))
       {
         SceneManager.LoadScene("Winscreen");
+      }
+      if(other.gameObject.CompareTag("Devroom"))
+      {
+        SceneManager.LoadScene("Devroom");
+      }
+      if(other.gameObject.CompareTag("Bingus"))
+      {
+        SceneManager.LoadScene("Bingusroom");
       }
    }
 }
